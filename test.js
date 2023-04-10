cab7dc883db9a7614532a7564cf2b82ecd871d39
Abhishek Pandey <abhishek.pandey@ucertify.com>
2023-04-10 12:11:38 +0530

    fw

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
-test('sum of two numbers', () => {
-    const sum = 5 + 3;
-    expect(sum).toBe(8);
-});
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
